28dbc1d8d6bb06f3903f9f1dc34d2fbc9795936d
Shumon Chattopadhyay <s.chattopadhyay89@gmail.com>
2017-08-22 18:54:14 -0400

    Adds comments that describe the functionality of each function

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,11 @@
 ## matrix, so that the calculation does not need to be done every time the 
 ## inverse is required.
 
-## makeCacheMatrix 
+## makeCacheMatrix has 4 functions:
+## 1. set allows the user to access the matrix that was input as an argument
+## 2. get allows the user to retrieve the matrix that was input as an argument
+## 3. setmatrix allows the user to save the matrix into ma, which is otherwise NULL
+## 4. getmatrix allows the user to retrieve the value saved in ma
 
 makeCacheMatrix <- function(x = matrix())
 {
@@ -28,7 +32,9 @@ makeCacheMatrix <- function(x = matrix())
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the cached inverse matrix is this was previously calculated
+## and already stored by the makeCacheMatrix function. Otherwise, it calculates
+## the inverse of the matrix x and stores it using makeCacheMatrix
 
 cacheSolve <- function(x, ...) 
 {
